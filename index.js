ad038bae77ec3006121e0ad92b8d39f15ed5bfa2
Eric Chu <leongzeno@gmail.com>
2020-05-29 09:02:12 -0700

    updated

--- index.js
+++ index.js
@@ -1,82 +1,76 @@
-import { mongo } from "../league-inhouse-client/server/module";
-
-function errors(code, data, message) {
-  let msg = ": "+message || "";
-  let status = {
-    status: code || null,
-    message: codes[code]+msg || null,
-    data: data || null
-  }
-  return status
-}
-
 const codes = {
   // 1xx
-  101: "Information is given",
-
-  // 2xx
-  200: "OK",
-  201: "Created",
-  202: "Accepted",
-  203: "Non-Authoritive Information",
-  204: "No Content",
-  205: "Rest Content",
-  206: "Partial Content",
-
-  // 3xx
-  300: "Multiple Choices",
-  301: "Moved Permanently",
-  302: "Found",
-  303: "See Other",
-  304: "Not Modified",
-  305: "Use Proxy",
-  306: "Unused",
-  307: "Temporarly Redirected",
-
-  //4xx
-  400: "Bad Request",
-  401: "Unauthorized",
-  402: "Payment Required",
-  403: "Forbidden",
-  404: "Not Found",
-  405: "Method Not Allowed",
-  406: "Not Acceptable",
-  407: "Proxy Authentication Required",
-  408: "Request Timeout",
-  409: "Conflict",
-  410: "Gone",
-  411: "Length Required",
-  412: "Precondition Failed",
-  413: "Request Entity Too Large",
-  414: "Request-url Too Long",
-  415: "Unsupported Media Type",
-  416: "Requested Range Not Satisfiable",
-  417: "Expectation Failed",
-  418: "ImATeapot",
-  421: "Missdirected Request",
-  422: "Unprocessable Entity",
-  423: "Locked",
-  424: "Failed Dependency",
-  425: "Unordered Collection",
-  426: "Upgrade Required",
-  428: "Precondition Required",
-  429: "Too Many Requests",
-  431: "Request Header Fields Too Large",
-  451: "Unavailable For Legal Reasons",
-
-  //5xx
-  500: "Internal Server Error",
-  501: "Not Implemented",
-  502: "Bad Gateway",
-  503: "Service Unavailable",
-  504: "Gateway Timeout",
-  505: "HTTP Version Not Supported",
-  506: "Variant Also Negotiates",
-  507: "Insufficient Storage",
-  508: "Loop Detected",
-  509: "Bandwidth Limit Exceeded",
-  510: "Not Extended",
-  511: "Network Authentication Required"
+  200: {
+    message: "OK",
+    method: "ok"
+  },
+  400: {
+    message: "Bad Request",
+    method: "bad"
+  },
+  401: {
+    message: "Unauthorized",
+    method: "notAuth"
+  },
+  403: {
+    message: "Forbidden",
+    method: "forbidden"
+  },
+  404: {
+    message: "Not Found",
+    method: "notFound"
+  },
+  408: {
+    message: "Request Timeout",
+    method: "timeout"
+  },
+  429: {
+    message: "Too Many Requests",
+    method: "limited"
+  },
+  500: {
+    message: "Internal Server Error",
+    method: "error"
+  },
+  503: {
+    message: "Service Unavailable",
+    method: "noService"
+  },
+  507: {
+    message: "Insufficient Storage",
+    method: "noStorage"
+  },
+  511: {
+    mesage: "Network Authentication Required",
+    method: "authRequired"
+  }
 }
 
-export default errors;
+export default function resCodes() {
+  return (req, res, next) => {
+    function init(cb) {
+      Object.keys(codes).forEach(i => {
+        Object.defineProperty(res, codes[i].method, {
+          enumerable: true,
+          configurable: true,
+          value: function(data, msg) {
+            try {
+              res.status(i).json({
+                status: i, 
+                message: codes[i].message+ ": "+ msg || codes[i].message,
+                data: data || ""
+              })
+            } catch(err) {
+              next(err)
+            }
+          },
+          writable: true
+        });
+      })
+      cb();
+    }
+    init(() => {
+      next();
+    })
+  }
+}
